d81e10c9c2297d14126d2c8bfcac55cdd9afe986
Robert <rwallhead@gmail.com>
2023-02-25 11:35:43 +1100

    Added auto-reboot of pi and following of output to deploy script

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,6 @@
 pipenv run python build\pyboard.py --device COM4 -f cp mfrc522.py :mfrc522.py
 pipenv run python build\pyboard.py --device COM4 -f cp neopixel.py :neopixel.py
 pipenv run python build\pyboard.py --device COM4 -f cp program.py :main.py
+pipenv run python build\pyboard.py --device COM4 --no-follow -c "import machine; machine.reset()"
+pipenv run python build\pyboard.py --device COM4 --follow
 pause
